577e772bd7428afb9919979e7ccfb3915b161dba
clara seijo <clasei@proton.me>
2024-08-28 21:39:56 +0200

    solved lab

--- index.js
+++ index.js
@@ -24,9 +24,27 @@ const part2 = "script";
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
 
 
+// let lastCharCap1 = part1.slice(-1).toUpperCase()
+// let lastCharCap2 = part2.slice(-1).toUpperCase()
+
+// console.log(lastCharCap1)
+// console.log(lastCharCap2)
+
+// let part1CamelTail1 = part1.slice(0, -1)
+// let part1CamelTail2 = part2.slice(0, -1)
+
+// console.log(part1CamelTail1)
+// console.log(part1CamelTail2)
+
+/* Better Option Here */
+cameLtaiLstrinG = part1.slice(0, -1) + part1.slice(-1).toUpperCase() + part2.slice(0, -1) + part2.slice(-1).toUpperCase()
+
 // Print the cameLtaiL-formatted string
 
+// cameLtaiLstrinG = part1CamelTail1 + lastCharCap1 + part1CamelTail2 + lastCharCap2
 
+/* Better Option Here */
+console.log(cameLtaiLstrinG)
 
 
 /*******************************************
@@ -36,10 +54,11 @@ const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
 
+tip = billTotal / 100 * 15
 
 // Print out the tipAmount
 
-
+console.log(tip)
 
 
 /*******************************************
@@ -48,9 +67,16 @@ const billTotal = 84;
 
 // Generate a random integer between 1 and 10 (inclusive)
 
+let randomNumber 
+
+// randomNumber = Math.floor(Math.random() * 10) // this shows an int num 0 - 9 (floor); the requeriments are 1 - 10
+
+randomNumber = Math.floor(Math.random() * 10) + 1 // adding '+ 1' the code matches the exercise requeriments (int  1 - 10)
+
 
 // Print the generated random number
 
+console.log(randomNumber)
 
 
 /*******************************************
@@ -61,16 +87,23 @@ const a = true;
 const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
-const expression1 = a && b;
+const expression1 = a && b; // false
+console.log(expression1)
 
-const expression2 = a || b;
+const expression2 = a || b; // true
+console.log(expression2)
 
-const expression3 = !a && b;
+const expression3 = !a && b; // false -> both operators must be true to return a true using && operator
+console.log(expression3)
 
-const expression4 = !(a && b);
+const expression4 = !(a && b); // true
+console.log(expression4)
 
-const expression5 = !a || !b;
+const expression5 = !a || !b; // true
+console.log(expression5)
 
-const expression6 = !(a || b);
+const expression6 = !(a || b); // false
+console.log(expression6)
 
-const expression7 = a && a;
+const expression7 = a && a; // true
+console.log(expression7)
